ed1145aa1f3c1e7253607cd565aa42a0e4553f8c
Shvechkova <anastasiashvechkova@gmail.com>
2022-12-10 13:57:23 +0400

    middle header

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,27 @@
+// footer
 $(document).ready(function () {
-    $('.footer__info__list').hide();
-    $('.footer__categori__list').hide();
-    $('.footer__client__list').hide();
+
+    $(window).width(function () {
+
+        if ($(this).width() < 768) {
+
+            $('.footer__info__list').hide();
+            $('.footer__categori__list').hide();
+            $('.footer__client__list').hide();
+
+        } else {
+
+            $('.footer__info__list').show();
+            $('.footer__categori__list').show();
+            $('.footer__client__list').show();
+
+        }
+
+    });
+   
 })
+
+
 
 
 $('.footer__info__title').on('click', function(){
@@ -21,4 +40,20 @@ $('.footer__client__title').on('click', function () {
     $('.footer__client__title--mobile').toggleClass('footer__client__title--mobile-active');
 })
 
+// header
+
+
+$('.translate__title').on('click', function () {
+
+    $('.translate__toggle__wrap').toggleClass('translate__toggle__wrap--active');
+    $('.translate__wrap').toggleClass('translate__wrap--active');
+
+})
+
+$('.header-middle__tel').on('click', function () {
+
+    $('.header-middle__tel-wrap').toggleClass('header-middle__tel-wrap--active');
+    $('.header-middle__tel-wrap--maxtel').toggleClass('header-middle__tel-wrap--maxtel--active');
+
+})
 
