626114edbbc9356650ab85cd7e5b69ece79f8dbf
Shvechkova <anastasiashvechkova@gmail.com>
2023-02-02 09:32:01 +0400

    menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,59 +1,74 @@
 // footer
 $(document).ready(function () {
+  $(window).width(function () {
+    if ($(this).width() < 768) {
+      $(".footer__info__list").hide();
+      $(".footer__categori__list").hide();
+      $(".footer__client__list").hide();
+    } else {
+      $(".footer__info__list").show();
+      $(".footer__categori__list").show();
+      $(".footer__client__list").show();
+    }
+  });
+});
 
-    $(window).width(function () {
+$(".footer__info__title").on("click", function () {
+  $(".footer__info__list").slideToggle();
+  $(".footer__info__title--mobile").toggleClass(
+    "footer__info__title--mobile-active"
+  );
+});
 
-        if ($(this).width() < 768) {
+$(".footer__categori__title").on("click", function () {
+  $(".footer__categori__list").slideToggle();
+  $(".footer__categori__title--mobile").toggleClass(
+    "footer__categori__title--mobile-active"
+  );
+});
 
-            $('.footer__info__list').hide();
-            $('.footer__categori__list').hide();
-            $('.footer__client__list').hide();
-
-        } else {
-
-            $('.footer__info__list').show();
-            $('.footer__categori__list').show();
-            $('.footer__client__list').show();
-
-        }
-
-    });
-   
-})
-
-
-
-
-$('.footer__info__title').on('click', function(){
-   
-    $('.footer__info__list').slideToggle();
-    $('.footer__info__title--mobile').toggleClass('footer__info__title--mobile-active');
-})
-
-$('.footer__categori__title').on('click', function () {
-    $('.footer__categori__list').slideToggle();
-    $('.footer__categori__title--mobile').toggleClass('footer__categori__title--mobile-active');
-})
-
-$('.footer__client__title').on('click', function () {
-    $('.footer__client__list').slideToggle();
-    $('.footer__client__title--mobile').toggleClass('footer__client__title--mobile-active');
-})
+$(".footer__client__title").on("click", function () {
+  $(".footer__client__list").slideToggle();
+  $(".footer__client__title--mobile").toggleClass(
+    "footer__client__title--mobile-active"
+  );
+});
 
 // header
 
+//header translate
+$(".translate__title").on("click", function () {
+  $(".translate__toggle__wrap").toggleClass("translate__toggle__wrap--active");
+  $(".translate__wrap").toggleClass("translate__wrap--active");
+});
 
-$('.translate__title').on('click', function () {
+// headet tell toggl
+$(".header-middle__tel").on("click", function () {
+  $(".header-middle__tel-wrap").toggleClass("header-middle__tel-wrap--active");
+  $(".header-middle__tel-wrap--maxtel").toggleClass(
+    "header-middle__tel-wrap--maxtel--active"
+  );
+});
+// header menu btn
+$(".header-middle__katalog-btn").on("click", function (event) {
+  event.stopPropagation();
+  $(".btn-img-katalog").toggleClass("btn-img-katalog--active");
+  $(".btn-img-katalog-close").toggleClass("btn-img-katalog-close--active");
 
-    $('.translate__toggle__wrap').toggleClass('translate__toggle__wrap--active');
-    $('.translate__wrap').toggleClass('translate__wrap--active');
+  $(".header-bottom-wrap").toggleClass("header-bottom-wrap--active-kat");
+  $(".main").toggleClass("main--active-kat");
+  $(".footer").toggleClass("footer--active-kat");
 
-})
+  $(".catalog").toggleClass("catalog--active");
+});
 
-$('.header-middle__tel').on('click', function () {
+$("body").on("click", function (event) {
+  $(".btn-img-katalog").removeClass("btn-img-katalog--active");
+  $(".btn-img-katalog-close").removeClass("btn-img-katalog-close--active");
 
-    $('.header-middle__tel-wrap').toggleClass('header-middle__tel-wrap--active');
-    $('.header-middle__tel-wrap--maxtel').toggleClass('header-middle__tel-wrap--maxtel--active');
+  $(".header-bottom-wrap").removeClass("header-bottom-wrap--active-kat");
+  $(".main").removeClass("main--active-kat");
+  $(".footer").removeClass("footer--active-kat");
 
-})
-
+  $(".catalog").removeClass("catalog--active");
+});
